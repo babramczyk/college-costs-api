662cb73e555ea8c5b1631547ab17108033a15c4d
babramczyk <bjabramczyk@gmail.com>
2019-05-11 09:58:55 -0700

    test: 404 errors for invalid "/colleges/..." routes

--- index.spec.js
+++ index.spec.js
@@ -11,6 +11,18 @@ test('returns a 404 for root endpoint', async () => {
     .expect(404)
 })
 
+test('returns a 404 for "/colleges/cost" endpoint', async () => {
+  await request(server)
+    .get('/colleges/cost')
+    .expect(404)
+})
+
+test('returns a 404 for "/colleges/:college_name" endpoint', async () => {
+  await request(server)
+    .get('/colleges/fake_college')
+    .expect(404)
+})
+
 test('returns a 200 for "/colleges/:college_name/cost" endpoint', async () => {
   await request(server)
     .get('/colleges/fake_college/cost')
