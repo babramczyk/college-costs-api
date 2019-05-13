7fa77d1b67b695594d60d3896ab59715375ae81e
babramczyk <bjabramczyk@gmail.com>
2019-05-12 19:38:37 -0500

    fix(jest.config): run in band to avoid race conditions

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // Run tests in parallel, since many of them are touching data files,
+  // and could corrupt other test files
+  runInBand: true,
   // Ignore built JSON data when triggering test runs in watch mode. Otherwise,
   // the build tests, which write/remove this file, will infinitely trigger
   // test runs
