5acf9c146de07a0f64085989fcc2fcc7cc68afb7
babramczyk <bjabramczyk@gmail.com>
2019-05-12 22:37:37 -0500

    fix: actually ensure that tests are runInBand (use CLI option)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // Run tests in parallel, since many of them are touching data files,
-  // and could corrupt other test files
-  runInBand: true,
   // Ignore built JSON data when triggering test runs in watch mode. Otherwise,
   // the build tests, which write/remove this file, will infinitely trigger
   // test runs
